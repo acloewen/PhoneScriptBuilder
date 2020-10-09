9e6979c99bf8f95b7b2aba62a7697b322b8bd04b
acloewen <adam.loewen@utsouthwestern.edu>
2020-10-09 14:18:30 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,10 +26,10 @@ async function onActivate(event) {
     console.info('Service worker: Activate');
 
     // Delete unused caches
-    //const cacheKeys = await caches.keys();
-    //await Promise.all(cacheKeys
-    //    .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
-    //    .map(key => caches.delete(key)));
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+        .map(key => caches.delete(key)));
 }
 
 async function onFetch(event) {
