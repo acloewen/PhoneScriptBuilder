9f4c85a5fa0ba35d8341cc33488bc4bd31ae639f
acloewen <adam.loewen@utsouthwestern.edu>
2020-10-12 09:47:05 -0500

    Update service-worker-assets.js

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -527,20 +527,20 @@
     {
       "hash": "sha256-yczp7uw8xTyEAy64zarIroiGICVJHi3VLIAvP\/GOYrM=",
       "url": "Icons\/trashcircle.svg"
-    }
-	/*,
+    },
     {
       "hash": "sha256-dp\/AHDQmpMcpYh54OGoy4tpe6UhJOvLVDO96wMPYeRk=",
       "url": "index.html"
-    },
+    }/*,
     {
       "hash": "sha256-OEq2bGikWjgd766Trw15HJZr+fS6boPjhWpKO+RqsPU=",
       "url": "js\/JavaScript.js"
-    }*/,
+    },
     {
       "hash": "sha256-U7LR+MUVkBbPsurwUK2vPX522wADYXzqSi1msRX17mU=",
       "url": "manifest.json"
-    },
+    }*/
+	,
     {
       "hash": "sha256-93vZW129xh7cfDpWG+7fvGaLF56GfZVSD5zmgrcVd7s=",
       "url": "mstile-150x150.png"
